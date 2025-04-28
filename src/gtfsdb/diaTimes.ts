a0b4b00bf064f390d8fca0907026f6c05191e08b
swallow3 <YamateKyuko@users.noreply.github.com>
2025-04-28 15:14:59 +0900

    jsonのパースに対応させようとしてみる

--- src/gtfsdb/diaTimes.ts
+++ src/gtfsdb/diaTimes.ts
@@ -16,40 +16,49 @@ const api = new dbAPI<{
     } = reqObj;
 
     const { results } = await db.prepare(`
-      select 
-        json_group_array(
-          json_object(
-            'feed_id', feed_id,
-            'trip_id', trip_id,
-            'stop_sequence', stop_sequence,
-            'stop_id', tim.stop_id,
-            'arrival_time', arrival_time,
-            'departure_time', departure_time,
-            'pickup_type', pickup_type,
-            'drop_off_type', drop_off_type,
-            'platform_code', platform_code,
-            'offset_time', offset_time
-          )
-        )
-      from stop_times as tim
-      inner join stop_patterns using (feed_id, pattern_id, stop_sequence)
-      inner join trips using (feed_id, trip_id)
-      WHERE 
-        tim.pattern_id = $1 and
-        service_id in (select service_id from calendar where date = $2)
-      group by trip_id
-      order by
-        feed_id,
-        trip_id,
-        stop_sequence;
+      select
+      json_group_array(arr) as results
+      from (
+        select 
+          json_group_array(
+            json_object(
+              'feed_id', feed_id,
+              'trip_id', trip_id,
+              'stop_sequence', stop_sequence,
+              'stop_id', tim.stop_id,
+              'arrival_time', arrival_time,
+              'departure_time', departure_time,
+              'pickup_type', pickup_type,
+              'drop_off_type', drop_off_type,
+              'platform_code', platform_code,
+              'offset_time', offset_time
+            )
+          ) as arr
+        from stop_times as tim
+        inner join stop_patterns using (feed_id, pattern_id, stop_sequence)
+        inner join trips using (feed_id, trip_id)
+        WHERE 
+          tim.pattern_id = $1 and
+          service_id in (select service_id from calendar where date = $2)
+        group by trip_id
+        order by
+          feed_id,
+          trip_id,
+          stop_sequence
+      );
       `,
     )
       .bind(...[patternId, date])
-      .all();
+      
+      .all()
+      
+      ;
     
     if (!results) return Response.json([]);
+
+    if (!results[0]['results']) return Response.json([]);
     
-    return Response.json(results);
+    return Response.json(JSON.parse(results[0]['results'] as string));
   },
 });
 
