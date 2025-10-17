015845058308486ff25d6ea721e706b763fff9c0
Yamakyu <70199568+YamateKyuko@users.noreply.github.com>
2025-10-17 11:48:16 +0900

    init.sql を更新

--- x/sql/map/init.sql
+++ x/sql/map/init.sql
@@ -1,8 +1,16 @@
 create schema if not exists busmap;
 
-drop table if exists busmap.patterns;
-create table busmap.pattern(
+drop table if exists busmap.maproutes;
+create table busmap.maproute(
+  maproute_id integer,
+  maproute_name text,
+  geom geometry(multilinestring, 4326)
+);
+
+drop table if exists busmap.mappatterns;
+create table busmap.mappattern(
   pattern_id integer,
+  maproute_id integer,
   stop_sequence integer,
   geom_sequence integer,
   feed_id text,
@@ -11,13 +19,21 @@ create table busmap.pattern(
   geom geometry(linestring, 4326)
 );
 
-drop table if exists busmap.stops;
-create table busmap.pattern(
+drop table if exists busmap.mapstations;
+create table busmap.stations(
+  mapstation_id integer,
+  mapstation_name text,
+  geom geometry(polygon, 4326)
+);
+
+drop table if exists busmap.mapstops;
+create table busmap.mapstops(
   pattern_id integer,
   stop_sequence integer,
+  maproute_id integer
   feed_id text,
   route_id text,
   route_name text,
   stop_name text,
-  geom geometry(linestring, 4326)
+  geom geometry(point, 4326)
 );
