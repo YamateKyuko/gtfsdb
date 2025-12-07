db2d2274c4c6536ae135ce9cf15f0399cb581732
yamakyu <YamateKyuko@users.noreply.github.com>
2025-12-07 09:23:33 +0900

    変更

--- src/gtfsdb/patternTimes.ts
+++ src/gtfsdb/patternTimes.ts
@@ -21,18 +21,18 @@ const api = new dbAPI({
       .prepare(`
 select
   pattern_id,
-  stop_sequence,
   ptn.feed_id,
-  trip_id,
-  arrival_time,
-  departure_time,
-  ptn.stop_id,
-  stop_headsign,
-  pickup_time,
-  drop_off_type,
+  route_id,
   route_name,
+  trip_id,
+  stop_sequence,
+  ptn.stop_id,
   stop_name,
   ptn.stop_headsign,
+  arrival_time,
+  departure_time,
+  pickup_type,
+  drop_off_type,
   zone_id
 from stop_patterns as ptn
 inner join stop_times using(pattern_id, stop_sequence)
--- x/sql/t.sql
+++ x/sql/t.sql
@@ -1,19 +1,28 @@
+-- select * from stop_patterns where pattern_id = 1 and stop_sequence = 1;
+
+
+
+
+
+
+
+
 select
   pattern_id,
-  stop_sequence,
   ptn.feed_id,
-  trip_id,
-  arrival_time,
-  departure_time,
-  ptn.stop_id,
-  stop_headsign,
-  pickup_time,
-  drop_off_type,
+  route_id,
   route_name,
+  trip_id,
+  stop_sequence,
+  ptn.stop_id,
   stop_name,
   ptn.stop_headsign,
+  arrival_time,
+  departure_time,
+  pickup_type,
+  drop_off_type,
   zone_id
 from stop_patterns as ptn
-inner join stop_times using(pattern_id, stop_sequence)
+inner join stop_times as tim using(pattern_id, stop_sequence)
 where (pattern_id, stop_sequence) in ((1,1))
 limit 100;
